8f04b10c542570e7f65860808a2547514e4c3cd7
BillyBernard <billybernard54@gmail.com>
2021-11-03 11:49:51 -0600

    tests still failing

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -11,7 +11,7 @@ const checkSchemeId = async (req, res, next) => {
   try {
     const existing = await db('schemes').where('scheme_id', req.params.scheme_id).first()
     if(!existing) {
-      next({ status: 404, message: `sheme with scheme_id ${req.params.scheme_id} not found` })
+      next({ status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found` })
     }
   } catch(err) {
     next(err)
